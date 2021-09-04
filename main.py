f5fa77dd431b0c7b7bad971462ca10b7f4c34a3c
Mark Beltran <markmbeltran@gmail.com>
2021-09-04 20:15:07 +0800

    Convert labels to oto

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ labels = [os.path.join(droppedFolder, file) for file in os.listdir(droppedFolder
 labels = [file for file in labels if os.path.splitext(file)[1] == ".txt"]
 
 csv.register_dialect('tsv', delimiter='\t')
+oto_lines = []
 for file in labels:
     filename = Path(file).stem
     print(f'Parsing line: {filename}')
@@ -58,8 +59,11 @@ for file in labels:
             if marker['text'] == 'start':
                 phonemes.append({"text": "start"})
             elif marker['text'] == 'end':
-                phonemes.append({"text": "end"})
-            else:
+                phonemes.append({
+                    "text": "end",
+                    "start": int(float(marker["start"])* 1000)
+                })
+            elif marker['text'] in settings['vowels'] or marker['text'] in settings['consonants']:
                 if marker_index + 1 >= len(file_data):
                     print('Missing end marker.')
                     input('Press any key to close.')
@@ -78,7 +82,74 @@ for file in labels:
                     print(f'Phoneme lacks stretch marker: {marker["text"]}')
                     input('Press any key to close.')
                     exit()
+            else:
+                print(f'Invalid phoneme: {marker["text"]}')
+                input('Press any key to close.')
+                exit()
             marker_index = marker_index + 1
 
     for current, next in zip(phonemes, phonemes[1:]):
-        print(f'{current["text"]} {next["text"]}')
+        alias = ""
+        offset = 0
+        fixed = 0
+        cutoff = 0
+        preutterance = 0
+        adjusted_overlap = 0
+        if current['text'] == 'start':
+            # -X
+            alias = "- " + next['text']
+            offset = next['start'] - 10
+            fixed = next['stretch start'] - next['start'] + 10
+            cutoff = 0 - (next['stretch end'] - next['start'] + 10)
+            preutterance = 10
+            adjusted_overlap = 0
+        elif current['text'] in settings['vowels']:
+            if next['start'] - current['stretch start'] < overlap * 2:
+                preutterance = next['start'] - current['stretch start']
+                adjusted_overlap = int(preutterance/2)
+            else:
+                preutterance = overlap * 2
+                adjusted_overlap = overlap
+            
+            offset = current['stretch end'] - adjusted_overlap
+
+            if next['text'] == 'end':
+                # V-
+                alias = current['text'] + " -"
+                fixed = preutterance + 10
+            else:
+                # VV and VC
+                alias = current['text'] + " " + next ['text']
+                fixed = next['stretch start'] - offset
+                cutoff = 0 - (next['stretch end'] - offset)
+        elif current['text'] in settings['consonants']:
+            offset = current['start']
+            preutterance = next['start'] - offset
+            if next['text'] == 'end':
+                # C-
+                alias = current['text'] + " -"
+                fixed = next['start'] - offset + 10
+            else:
+                if next['text'] in settings['vowels']:
+                    # CV
+                    alias = current['text'] + next['text']
+                elif next['text'] in settings['consonants']:
+                    # CC
+                    alias = current['text'] + " " + next ['text']
+                fixed = next['stretch start']
+                cutoff = 0 - (next['stretch end'] - offset)
+                if current['stretch end'] - offset < int(preutterance/2):
+                    # stop cons
+                    adjusted_overlap = current['stretch end'] - offset
+                else:
+                    # stretch cons
+                    adjusted_overlap = int(preutterance/2)
+        
+        oto_lines.append(f'{filename}.wav={alias},{offset},{fixed},{cutoff},{preutterance},{adjusted_overlap}')
+
+oto_file = open(os.path.join(droppedFolder, "oto.ini"), "w")
+oto_file.writelines(oto_lines)
+oto_file.close()
+print('Generated OTO has been saved.')
+input('Press any key to close.')
+exit()
