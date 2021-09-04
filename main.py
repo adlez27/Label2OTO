4ba8f277537b58d644efe20ed14dfb299134070e
Mark Beltran <markmbeltran@gmail.com>
2021-09-04 12:53:38 +0800

    read files

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from genericpath import isdir
+import sys
 import os
 import json
+import csv
+
+try:
+    droppedFolder = sys.argv[1]
+    if (not os.path.isdir(droppedFolder)):
+        print("Please drag and drop a folder onto main.py.")
+        input("Press any key to close.")
+        exit()
+except IndexError:
+    print("Please drag and drop a folder onto main.py.")
+    input("Press any key to close.")
+    exit()
 
 print('Select reclist')
 lists = os.listdir("settings")
@@ -10,3 +24,13 @@ selected_list = lists[int(input(": ")) - 1]
 settings = {}
 with open("settings\\" + selected_list) as file:
     settings = json.loads(file.read())
+
+labels = [os.path.join(droppedFolder, file) for file in os.listdir(droppedFolder)]
+labels = [file for file in labels if os.path.splitext(file)[1] == ".txt"]
+
+csv.register_dialect('tsv', delimiter='\t')
+for file in labels:
+    with open(file, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file, dialect='tsv', fieldnames=["start", "end", "text"])
+        for row in csv_reader:
+            print(f'Start time: {row["start"]}\nEnd time: {row["end"]}\nLabel text: {row["text"]}')
